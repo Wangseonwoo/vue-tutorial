0b5956b830bfe320c589bbbee69cc9b6d1ffc135
Wangseonwoo <dmb05007@naver.com>
2023-12-09 14:48:37 +0900

    vue-pr1

---
+++ src/practice/App-ref.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ message }}
+    <button @click="addMessage">add click</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup () {
+    const message =ref('Hello vue!');
+    const addMessage = () => {
+      message.value = message.value +'!';
+    }
+    return { message, addMessage }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/practice/App-toRef-toRefs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>author: {{ author }}</p>
+    <p>title: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive, toRef, toRefs } from 'vue';
+
+export default {
+  setup () {
+    const book = reactive({
+      author: 'Sunwoo',
+      year: '2023',
+      title: 'Vue3 Pr',
+      description: '당신은 뷰3를 공부하고 있습니다.',
+      price: '무료',
+    });
+    // 반응형을 잃어버리는 구조 분해할당
+    const { author, title } = book;
+    // 반응형을 유지하는 구조 분해할당
+    const { year, description } = toRefs(book);
+    const price = toRef(book, 'price');
+    console.log(typeof author);
+    // console.log(typeof year);
+    // console.log(typeof price);
+
+    return { author, title, price, description, year };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
